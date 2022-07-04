4e0465ea53d6ef5cabd9af8753ca8dbbdb2753e1
gabarnettjr <gabarnettjr@gmail.com>
2022-07-04 12:39:42 -0600

    Add files via upload

--- packages/timeStepping/explicit/rk/rk3.jl
+++ packages/timeStepping/explicit/rk/rk3.jl
@@ -1,13 +1,16 @@
 
-function rk3!(t, U, odefun!, dt, q1, q2)
+function rk3!(t, U, odefun!, dt, q1, q2, Utmp)
 
-    q1 = odefun!(t,        U,           q1)
-    q2 = odefun!(t+dt/3,   U+dt/3*q1,   q2)
-    q2 = odefun!(t+2*dt/3, U+2*dt/3*q2, q2)
+    q1 = odefun!(t, U, q1)
 
-    U = U + dt/4 * (q1 + 3*q2)
+    Utmp = U + dt/3 * q1
+    q2 = odefun!(t + dt/3, Utmp, q2)
 
-    return U
+    Utmp = U + 2*dt/3 * q2
+    q2 = odefun!(t + 2*dt/3, Utmp, q2)
+
+    Utmp = U + dt/4 * (q1 + 3 * q2)
+
+    return Utmp
 
 end
-
